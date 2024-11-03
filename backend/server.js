3ff6981a10a98e95aa245866bc439a93655a84f3
Paras Sareen <154830999+parassareen4@users.noreply.github.com>
2024-11-03 08:29:04 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const io = socketIo(server, {
 app.use(cors());
 
 // MongoDB connection
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect("mongodb+srv://parassareen:parassareen1@cluster0.qnxcu.mongodb.net/", { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('MongoDB connection error:', err));
 
